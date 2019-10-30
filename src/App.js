5bbe2743d2087272eac15672d905e6151cd74194
Kamil <kamil.tomala@ath.pl>
2019-10-30 15:32:57 +0100

    Outputting list using map

--- src/App.js
+++ src/App.js
@@ -43,18 +43,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          >
-            My Hobbies: Running
-          </Person>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, "Kamillo")}
-            changed={this.nameChangedHandler}
-          />
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />;
+          })}
         </div>
       );
     }
