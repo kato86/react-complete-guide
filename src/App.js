eeefd06370028517750e5dfa4ea44244a9d2c787
Kamil <kamil.tomala@ath.pl>
2019-10-31 13:31:46 +0100

    Flexible lists

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import Person from "./Person/Person";
 
 class App extends Component {
   state = {
-    persons: [{ name: "Kamil", age: 33 }, { name: "Ania", age: 35 }],
+    persons: [
+      { id: "1", name: "Ka", age: 33 },
+      { id: "2", name: "A", age: 35 },
+      { id: "3", name: "M", age: 3 },
+      { id: "4", name: "MM", age: 0 }
+    ],
     showPersons: false
   };
 
@@ -14,13 +19,21 @@ class App extends Component {
     this.setState({ persons: persons });
   };
 
-  nameChangedHandler = event => {
-    this.setState({
-      persons: [
-        { name: "Kamil", age: 33 },
-        { name: event.target.value, age: 35 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -48,7 +61,8 @@ class App extends Component {
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
-                key={index}
+                key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
