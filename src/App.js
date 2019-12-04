2dadcc209c99f3a42d382e483708bb45541dd6a4
Kamil <tomala.kamil@gmail.com>
2019-12-04 15:17:25 +0100

    Change styled-component to CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from "react";
-import styled from 'styled-components';
-import "./App.css";
+import classes from "./App.module.css";
 import Person from "./Person/Person";
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font-size: 18px;
-  border: 2px solid lightblue;
-  padding: 10px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'ligthgreen'};
-    color: black;
-  }
-`;
 
 class App extends Component {
   state = {
@@ -75,21 +60,25 @@ class App extends Component {
       );
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push("red");
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push("bold");
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+        <p className={assignedClasses.join(" ")}>This is really working!</p>
+        <button
+          className={classes.Button}
+          alt={this.state.showPersons}
+          onClick={this.togglePersonsHandler}
+        >
           Switch Name
-        </StyledButton>
+        </button>
         {persons}
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,30 +1,15 @@
 import React from "react";
-import styled from 'styled-components';
-// import "./Person.css";
-
-const StyledDiv = styled.div`
-  border: 1px solid #eee;
-  box-shadow: 0 2px 5px #ccc;
-  padding: 10px;
-  width: 400px;
-  text-align: center;
-  margin: auto;
-  display: block;
-
-  @media (min-width: 500px) {
-    width: 450px;
-  }
-`
+import classes from "./Person.module.css";
 
 const person = props => {
   return (
-    <StyledDiv>
+    <div className={classes.Person}>
       <p onClick={props.click}>
         I'm a {props.name} and I am {props.age} years old.
       </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />    
-    </StyledDiv>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
   );
 };
 
