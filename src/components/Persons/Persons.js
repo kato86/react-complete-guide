6b0c10504d09c5138cf21b30423f94d15414e6d3
MATRIX\Kamil <tomala.kamil@gmail.com>
2019-12-08 22:33:21 +0100

    Update lifecicle

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,6 +3,28 @@ import React, { Component } from "react";
 import Person from "./Person/Person";
 
 class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log("[Persons.js] getDerivedStateFropProps");
+  //   return state;
+  // }
+
+  // componentWillReceiveProps(props) {
+  //   console.log("[Persons.js] componentWillReceiveProps", props);
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] getShapshotBeforeUpdate");
+    return { message: "Snapshot!" };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Persons.js] componentDidUpdate", snapshot);
+  }
   render() {
     console.log("[Persons.js] rendering...");
     return this.props.persons.map((person, index) => {
